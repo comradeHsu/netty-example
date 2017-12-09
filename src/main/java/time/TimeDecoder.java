c213b0f129b428949144169f45c0e84718261e84
knockinHeavenDoor <61239289@qq.com>
2017-12-09 23:33:18 +0800

    分支更换

--- src/main/java/time/TimeDecoder.java
+++ src/main/java/time/TimeDecoder.java
@@ -20,5 +20,5 @@ public class TimeDecoder extends ByteToMessageDecoder{ // (1)
 //    2.ByteToMessageDecoder在接收到新数据时，使用内部维护的累积缓冲区调用decode()方法。
 //    3.decode()可以决定在累积缓冲区中没有足够数据的情况下不添加任何东西。 当接收到更多数据时，ByteToMessageDecoder将再次调用decode()。
 //    4.如果decode()将对象添加到out，则意味着解码器成功地解码了消息。 ByteToMessageDecoder将丢弃累积缓冲区的读取部分。要记住，不需要
-//    解码多个消息。 ByteToMessageDecoder将继续调用decode()方法，直到它没有再有任何东西添加。
+//      解码多个消息。 ByteToMessageDecoder将继续调用decode()方法，直到它没有再有任何东西添加。
 }
